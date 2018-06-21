c0fddfca727009e9dabb75ab3850db55577698b2
jinsol831 <jennyjwon@gmail.com>
2018-06-20 21:22:42 -0500

    Adding new pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,38 +1,37 @@
-
 // stock data for friendList
 var friendList = [{
-    "name": "Bulbasaur",
-    "photo": "https://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png",
+    "name": "Goldendoodle",
+    "photo": "https://www.google.com/search?q=goldendoodle&tbm=isch&source=iu&ictx=1&fir=CdIQAFTrMth-4M%253A%252CNwL-jvFwHBkl5M%252C_&usg=__23yTRJtArtQTuMq5Y95XmAcewo0%3D&sa=X&ved=0ahUKEwidzfbU2OPbAhUZ8YMKHVlPCf8Q_h0I-AEwGA#imgrc=CdIQAFTrMth-4M:",
     "scores": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 },
 {
-    "name": "Pikachu",
-    "photo": "https://cdn.bulbagarden.net/upload/0/0d/025Pikachu.png",
+    "name": "Doodle Puppy!",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjS1M_l2OPbAhVn2IMKHeWmCjsQjRx6BAgBEAU&url=https%3A%2F%2Fwww.swissridgekennels.com%2Fbreeds%2Fgoldendoodles%2F&psig=AOvVaw01_UhzGwhiEdLqtcrWFobl&ust=1529633956398375",
     "scores": [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
 
 
 },
 {
-    "name": "Snorlax",
-    "photo": "https://cdn.bulbagarden.net/upload/f/fb/143Snorlax.png",
+    "name": "Bernedoodle Puppy!",
+    "photo": "https://www.rockymtnbernedoodles.com/uploads/8/7/1/3/87136022/s925891007969739195_p12_i1_w1080.jpeg",
     "scores": [5, 5, 5, 5, 5, 1, 5, 5, 5, 5]
 },
 
 {
-    "name": "Meowth",
-    "photo": "https://cdn.bulbagarden.net/upload/thumb/d/d6/052Meowth.png/500px-052Meowth.png",
+    "name": "Bernedoodle",
+    "photo": "http://www.sheepadoodlepuppiesforsale.com/uploads/1/0/5/7/10574597/fullsizeoutput-3b9_1_orig.jpeg",
     "scores": [5, 5, 5, 5, 5, 1, 5, 5, 5, 5]
 },
 
 {
-    "name": "Bayleaf",
-    "photo": "https://cdn.bulbagarden.net/upload/thumb/c/ca/153Bayleef.png/1200px-153Bayleef.png",
+    "name": "Aussidoodle",
+    "photo": "https://www.hellowdog.com/wp-content/uploads/2018/04/aussiedoodle-pictures.jpg",
     "scores": [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 },
 
 {
-    "name": "Mew",
-    "photo": "https://cdn.bulbagarden.net/upload/thumb/b/b1/151Mew.png/1200px-151Mew.png",
+    "name": "Sheepadoodle",
+    "photo": "https://animalso.com/wp-content/uploads/2018/04/sheepadoodle2-372x372.jpg",
     "scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 },
 ]; // end of array
